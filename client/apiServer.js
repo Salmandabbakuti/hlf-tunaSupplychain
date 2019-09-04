709603737a58c42f04badbd03d3ca0ffe0ba7599
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-04 09:16:09 +0530

    Added new tuna add function

--- client/apiServer.js
+++ client/apiServer.js
@@ -67,6 +67,40 @@ app.get('/api/queryTuna/:tuna_index', async function (req, res) {
         process.exit(1);
     }
 });
+
+app.post('/api/addTuna/', async function (req, res) {
+ try {
+// Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+// Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists('user1');
+        if (!userExists) {
+            console.log('An identity for the user "user1" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+// Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
+// Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+// Get the contract from the network.
+        const contract = network.getContract('mycc');
+// Submit the specified transaction.
+        await contract.submitTransaction('addTuna', req.body.tunaId, req.body.vessel, req.body.location, req.body.timestamp, req.body.owner);
+        console.log('Transaction has been submitted');
+        res.send('Transaction has been submitted');
+// Disconnect from the gateway.
+        await gateway.disconnect();
+} catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+
+})
+
 app.put('/api/changeTunaOwner/:tuna_index', async function (req, res) {
  try {
 // Create a new file system based wallet for managing identities.
