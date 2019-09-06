180b5706fa48749665f2ecea48cf695bbced8cf8
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-06 17:54:57 +0530

    Url body Support

--- client/apiServer.js
+++ client/apiServer.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
-app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Setting for Hyperledger Fabric
 const { FileSystemWallet, Gateway } = require('fabric-network');
