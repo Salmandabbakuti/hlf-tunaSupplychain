c13a371150195d58e5509b922b90e3ee1903602b
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-01-16 15:55:32 +0530

    Update mychaincode.js

--- chaincode/newcc/mychaincode.js
+++ chaincode/newcc/mychaincode.js
@@ -130,7 +130,55 @@ async richQuery(stub, args, thisClass){
         let queryResults = await method(stub, JSON.stringify(queryString), thisClass);
         return queryResults; //shim.success(queryResults);
     }
+async getQueryResultForQueryString(stub, queryString, thisClass) {
 
+        console.info('- getQueryResultForQueryString queryString:\n' + queryString)
+        let resultsIterator = await stub.getQueryResult(queryString);
+        let method = thisClass['getAllResults'];
+    
+        let results = await method(resultsIterator, false);
+    
+        return Buffer.from(JSON.stringify(results));
+}
+	
+async getAllResults(iterator, isHistory) {
+        let allResults = [];
+        while (true) {
+          let res = await iterator.next();
+    
+          if (res.value && res.value.value.toString()) {
+            let jsonRes = {};
+            console.log(res.value.value.toString('utf8'));
+    
+            if (isHistory && isHistory === true) {
+              jsonRes.TxId = res.value.tx_id;
+              jsonRes.Timestamp = res.value.timestamp;
+              jsonRes.IsDelete = res.value.is_delete.toString();
+              try {
+                jsonRes.Value = JSON.parse(res.value.value.toString('utf8'));
+              } catch (err) {
+                console.log(err);
+                jsonRes.Value = res.value.value.toString('utf8');
+              }
+            } else {
+              jsonRes.Key = res.value.key;
+              try {
+                jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
+              } catch (err) {
+                console.log(err);
+                jsonRes.Record = res.value.value.toString('utf8');
+              }
+            }
+            allResults.push(jsonRes);
+          }
+          if (res.done) {
+            console.log('end of data');
+            await iterator.close();
+            console.info(allResults);
+            return allResults;
+          }
+        }
+    }
   async changeTunaOwner(stub, args) {
     console.info('============= START : changing Tuna Owner ===========');
     if (args.length != 2) {
